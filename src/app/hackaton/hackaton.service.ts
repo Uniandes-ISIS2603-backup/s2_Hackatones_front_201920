d874ee300b52b48c2514e7228d58e6eb78b9b825
Esteban Cárdenas <ne.cardenas@uniandes.edu.co>
2019-12-05 05:47:34 -0500

    Doc

--- src/app/hackaton/hackaton.service.ts
+++ src/app/hackaton/hackaton.service.ts
@@ -1,47 +1,43 @@
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+    import {Injectable} from '@angular/core';
+    import {Observable} from 'rxjs';
+    import {HttpClient} from '@angular/common/http';
 
-import {Hackaton} from './hackaton';
-import {HackatonDetail} from './hackaton-detail';
-import { LugarDetail } from '../lugar/lugar-detail';
-import { Equipo } from '../equipo/equipo';
-import { EquipoDetail } from '../equipo/equipo-detail';
-
-
-const API_URL = '../../assets/';
-const hackatones = 'hackatones.json';
+    import {Hackaton} from './hackaton';
+    import {HackatonDetail} from './hackaton-detail';
+    import { LugarDetail } from '../lugar/lugar-detail';
+    import { Equipo } from '../equipo/equipo';
+    import { EquipoDetail } from '../equipo/equipo-detail';
 
 
-/**
-* The service provider for everything related to hackatones
-*/
-@Injectable()
-export class HackatonService {
-    getHackaton() : Observable<Hackaton[]> {
-        return this.http.get<Hackaton[]>(API_URL + hackatones);
-    }
+    const API_URL = '../../assets/';
+    const hackatones = 'hackatones.json';
 
-    getHackatonDetail(hackatonId): Observable<HackatonDetail> {
-        return this.http.get<HackatonDetail>(API_URL + "hackaton-" + hackatonId+".json");
-    }
-
-    createHackaton(hackaton): Observable<HackatonDetail> {
-        return this.http.post<HackatonDetail>(API_URL + hackatones, hackaton);
-    }
-
-    getLugar(hackatonId) : Observable<LugarDetail> {
-        return this.http.get<LugarDetail>(API_URL + "hackatones/" +hackatonId + "/lugar/" +  hackatonId);
-    }
-
-    getEquipos(hackatonId): Observable<EquipoDetail> {
-        return this.http.get<EquipoDetail>(API_URL + "hackatones/" + hackatonId + "/equipo")
-    }
 
     /**
-    * Constructor of the service--
-    * @param http The HttpClient - This is necessary in order to perform requests
+    * The service provider for everything related to hackatones
     */
-    constructor(private http: HttpClient) {}
+    @Injectable()
+    export class HackatonService {
+        getHackaton() : Observable<Hackaton[]> {
+            return this.http.get<Hackaton[]>(API_URL + hackatones);
+        }
 
-}
+        getHackatonDetail(hackatonId): Observable<HackatonDetail> {
+            return this.http.get<HackatonDetail>(API_URL + "hackaton-" + hackatonId+".json");
+        }
+
+        createHackaton(hackaton): Observable<HackatonDetail> {
+            return this.http.post<HackatonDetail>(API_URL + hackatones, hackaton);
+        }
+
+        getLugar(hackatonId) : Observable<LugarDetail> {
+            return this.http.get<LugarDetail>(API_URL + "hackatones/" +hackatonId + "/lugar/" +  hackatonId);
+        }
+
+        /**
+        * Constructor of the service--
+        * @param http The HttpClient - This is necessary in order to perform requests
+        */
+        constructor(private http: HttpClient) {}
+
+    }
